ff64e0da939dbc68852bb08dfd2cb07830e5e36f
Nikolai <nvanbaak@gmail.com>
2020-09-22 20:26:40 -0700

    search now grabs geoname ID with associated goodies

--- app.js
+++ app.js
@@ -8,16 +8,25 @@ $("#search-button").on("click", function(event) {
 
     // Create query URL
     var queryURL = "https://api.teleport.org/api/cities/?search=" + cityName;
-    
-    // var queryURL = "https://api.teleport.org/api/urban-areas/" + cityName + "/cities/";
 
-
+    // Use query URL to make first AJAX request
     $.ajax({
         url:queryURL,
         method:"GET"
     }).then( function(response) {
 
-        console.log(response);
+        // Get url with geoname ID
+        var geonameURL = response._embedded["city:search-results"][0]._links["city:item"].href;
+
+        // Make AJAX request with new URL
+        $.ajax({
+            url:geonameURL,
+            method:"GET"
+        }).then( function(response) {
+    
+            console.log(response);
+        
+        })
     
     })
 
