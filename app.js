3853f4063fb2e9d15ecab5b46892cccbe2eeb866
Nikolai <nvanbaak@gmail.com>
2020-09-22 20:03:44 -0700

    playing around with teleport

--- app.js
+++ app.js
@@ -3,8 +3,22 @@
 $("#search-button").on("click", function(event) {
     event.preventDefault();
 
+    // Grab city name from input bar
     var cityName = $("#search-bar").val();
 
-    console.log(cityName);
+    // Create query URL
+    var queryURL = "https://api.teleport.org/api/cities/?search=" + cityName;
+    
+    // var queryURL = "https://api.teleport.org/api/urban-areas/" + cityName + "/cities/";
+
+
+    $.ajax({
+        url:queryURL,
+        method:"GET"
+    }).then( function(response) {
+
+        console.log(response);
+    
+    })
 
 })
